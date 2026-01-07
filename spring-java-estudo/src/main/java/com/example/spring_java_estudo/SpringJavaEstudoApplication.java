23889dfe8735767452a4e6073dac173f3df0fb17
Yagorocha9 <yagodsrocha@gmail.com>
2026-01-07 00:43:22 -0300

    Projeto criado

---
+++ spring-java-estudo/src/main/java/com/example/spring_java_estudo/SpringJavaEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_java_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaEstudoApplication.class, args);
+	}
+
+}
---
+++ spring-java-estudo/src/test/java/com/example/spring_java_estudo/SpringJavaEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_java_estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
